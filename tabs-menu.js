b45c5c7c663134be5606893b0242e69d4aff9932
Thu <hyatmyat779@gmail.com>
2023-11-01 19:17:34 +0630

    add: tabs-menu

--- tabs-menu.js
+++ tabs-menu.js
@@ -3,9 +3,16 @@ const sliderTag = document.querySelector(".slider");
 
 const tabs = ["home", "services", "about us", "contact us", "login"];
 
+const handleTagChange = (event) => {
+  console.log(event.target.id);
+};
 for (let i = 0; i < tabs.length; i++) {
   const liTag = document.createElement("li");
   liTag.append(tabs[i].toUpperCase());
   liTag.id = i;
+  liTag.addEventListener("click", handleTagChange);
   ulTag.append(liTag);
+  if (i === 0) {
+    sliderTag.style.width = liTag.offsetWidth + "px";
+  }
 }
