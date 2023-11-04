1bec9d285160ba333cd218085de1a40aabb1b7c1
Thu <hyatmyat779@gmail.com>
2023-11-04 15:57:49 +0630

    add: toast-alert

---
+++ toast-alert.js
@@ -0,0 +1,9 @@
+const buttonTag = document.querySelector(".btn");
+const parentTag = document.querySelector(".parent");
+
+const openToastAlert = () => {
+  const ToastAlertTag = document.createElement("div");
+};
+buttonTag.addEventListener("click", () => {
+  openToastAlert();
+});
