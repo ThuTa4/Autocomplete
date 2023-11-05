bfe2acf403503448afd85514b4df57494625bc7c
Thu <hyatmyat779@gmail.com>
2023-11-05 19:31:47 +0630

    add: toast-alert

--- toast-alert.js
+++ toast-alert.js
@@ -7,6 +7,13 @@ const openToastAlert = () => {
   toastAlertTag.append("Your file was successfully uploaded.");
   toastAlertTag.classList.add("toastAlert");
   parentTag.append(toastAlertTag);
+  toastAlertTag.style.top = `-${toastAlertTag.offsetHeight}px`;
+  setTimeout(() => {
+    toastAlertTag.style.top = `0px`;
+  }, 100);
+  setTimeout(() => {
+    toastAlertTag.style.top = `-${toastAlertTag.offsetHeight}px`;
+  }, 2000);
 };
 buttonTag.addEventListener("click", () => {
   openToastAlert();
